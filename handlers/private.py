8137bd35d995e21911ccf8a25c03db04b34bb983
RiderFA <73771216+RiderFA@users.noreply.github.com>
2021-04-18 10:30:37 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -9,22 +9,29 @@ from helpers.filters import other_filters2
 async def start(_, message: Message):
     await message.reply_text(
         f"""I am **{bn}** !!
-I let you play music in your group's voice chat pro owner @backup_pista123 😉
+I let you play music in your group's voice chat 😉
+Currently I am under a private vc music player ⏩
+To add me take permission from [Owner](https://t.me/backup_pista123)
+Dm him directly no tension ❤️
 The commands I currently support are:
 ⚜️ /play - __Plays the replied audio file or YouTube video through link.__
 ⚜️ /pause - __Pause Voice Chat Music.__
 ⚜️ /resume - __Resume Voice Chat Music.__
 ⚜️ /skip - __Skips the current Music Playing In Voice Chat.__
 ⚜️ /stop - __Clears The Queue as well as ends Voice Chat Music.__
+⚜️ /song (song name) - __To search song and send song directly.__
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "Group 💬", url="https://t.me/INDIANN_CHAT_GROUP"
+                        "Group 💬", url="https://t.me/RIDERIANS"
                     ),
                     InlineKeyboardButton(
                         "Channel 📣", url="https://t.me/riderians"
+                    ),
+                    InlineKeyboardButton(
+                        "Owner 👑", url="https://t.me/backup_pista123"
                     )
                 ]
             ]
